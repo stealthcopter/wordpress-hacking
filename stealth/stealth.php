cc2097c706c783f6182bd77a6b5c5a8e5daebd9a
Mat Rollings <admin@stealthcopter.com>
2024-10-16 14:46:24 +0100

    fixed it

--- stealth/stealth.php
+++ stealth/stealth.php
@@ -8,51 +8,61 @@
  * Author URI: https://sec.stealthcopter.com/
  */
 
-const STEALTH_PLUGIN_FILE = __FILE__;
-const STEALTH_PLUGIN_PATH = __DIR__;
+if (!defined('STEALTH_PLUGIN_FILE')) {
+    define('STEALTH_PLUGIN_FILE', __FILE__);
+}
+
+if (!defined('STEALTH_PLUGIN_PATH')) {
+    define('STEALTH_PLUGIN_PATH', __DIR__);
+}
 
 require_once 'inc/loader.php';
-require_once 'config.php';
-require_once 'api.php';
-
-require_once 'inc/code.php';
-require_once 'inc/views.php';
 
 // Load the PHP object gadget
 require_once 'payloads/php_obj.php';
 
-function render_page(){
-    $page = 'index';
-    if (isset($_REQUEST['stealth_page'])) {
-        $page = basename($_REQUEST['stealth_page']);
+if (!function_exists('stealth_render_page')) {
+    function stealth_render_page()
+    {
+        $page = 'index';
+        if (isset($_REQUEST['stealth_page'])) {
+            $page = basename($_REQUEST['stealth_page']);
+        }
+
+        include 'inc/templates/header.php';
+
+        echo '<div class="content mt-4">';
+        // You can path traverse here if you like, no stress
+        include "inc/pages/$page.php";
+        echo '</div>';
+
+        include 'inc/templates/footer.php';
     }
-
-    include 'inc/templates/header.php';
-
-    echo '<div class="content mt-4">';
-    // You can path traverse here if you like, no stress
-    include "inc/pages/$page.php";
-    echo '</div>';
-
-    include 'inc/templates/footer.php';
 }
 
-if (is_installed_to_wordpress()){
-    // Intercept urls
-    if ( function_exists( 'add_action' ) ) {
-        add_action('parse_request', 'stealth_url_handler');
-        function stealth_url_handler( $query_args ) {
-            if( strstr( $_SERVER["REQUEST_URI"], "/wp-content/plugins/stealth/" )){
-                // Don't intercept
-            }
-            else if( strstr( $_SERVER["REQUEST_URI"], STEALTH_PERMALINK_PATH )){
-                // Intercept
-                render_page();
-                die();
-            }
+function init_stealth()
+{
+    require_once 'config.php';
+    require_once 'api.php';
+
+    require_once 'inc/code.php';
+    require_once 'inc/views.php';
+}
+
+if (is_installed_to_wordpress()) {
+    add_action('init', 'init_stealth');
+
+    add_action('parse_request', 'stealth_url_handler');
+    function stealth_url_handler($query_args)
+    {
+        if (strstr($_SERVER["REQUEST_URI"], STEALTH_PERMALINK_PATH)) {
+            // Intercept
+            stealth_render_page();
+            die();
         }
     }
-}
-else{
-    render_page();
+
+} else {
+    init_stealth();
+    stealth_render_page();
 }
